5927949741e43c82b801ba5dd0d8875b036b5342
mehmet-cloud <mehmetbozdag155@gmail.com>
2021-04-03 00:30:51 -0400

    Completed Aritmetic Lab

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
+let num1 = 2;
+let num2 = 31;
 
-// Write your code here
+const multiply = num1*num2;
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+
+let num3=14;
+let num4=5;
+let mod = num3 % num4; 
+
+var max = Math.max(1, 2, 3, 4, 20);
+
+
+
+
+
+
+
+
+
+
+
+
+
